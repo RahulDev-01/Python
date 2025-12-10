de4c9f6d2f16150328902fe03512a221e9b59b91
S.Rahul <s.rahul5116@outlook.com>
2025-12-10 18:49:11 +0530

    loops.py added

---
+++ loops.py
@@ -0,0 +1,14 @@
+# For Loop
+
+# for i in range(10000001):
+    # print(i)
+# Loop to Track the elements in the list  
+# x= [1,2,3,4,5,6,7,8,9,10,'End']
+# for i in range(len(x)):
+#     print(x[i])
+
+# While Loop
+i=0
+while i<100:
+    i+=1
+    print(i)
