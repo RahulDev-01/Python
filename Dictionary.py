0d2e51439d579b303a84c06459dc3c6a61f8e93a
S.Rahul <s.rahul5116@outlook.com>
2025-12-11 19:24:12 +0530

    Add Dictionary.py with examples of dictionary operations

---
+++ Dictionary.py
@@ -0,0 +1,15 @@
+x={
+    'Name':"Rahul",
+    'Age':24,
+    'City':"Mumbai"
+}
+print(x)
+# Accessing Values
+print(x['Name'])
+print(x.get('Age'))
+# Adding Key-Value Pair
+x['Profession']="Developer"
+print(x)
+# Modifying Values
+x['Age']=25
+print(x)
