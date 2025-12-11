441ee1888639eaf1ab46f329192db768b8aef28c
S.Rahul <s.rahul5116@outlook.com>
2025-12-11 19:19:15 +0530

    Add Sets.py with set operations examples

---
+++ Sets.py
@@ -0,0 +1,11 @@
+x= set() 
+# print(x)
+s={1,2,3,4,5,6}
+s1={7,8,9,10,11,12}
+# print(s)
+# x.add(7)
+# x.remove(7)
+# s.union(s1)
+print(s.union(s1))
+print(s.intersection(s1))
+print(s.difference(s1))
