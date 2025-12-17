c7554ad91d60005c28cc2409da1c292fbd9b433f
S.Rahul <s.rahul5116@outlook.com>
2025-12-17 16:39:21 +0530

    Added f-string example in fstring.py file

---
+++ fstring.py
@@ -0,0 +1,10 @@
+# f-string in Python
+# It is used to format strings in a more readable way 
+x=f'Hello,{"World"}'
+print(x)
+# f-string in Python with variables
+
+name='Rahul'
+age=21
+info=f'My name is {name} and I am {age} years old.'
+print(info)
