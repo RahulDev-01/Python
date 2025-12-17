4a0ba5dc426d6f90b4c3752821ccff3c0be47bd6
S.Rahul <s.rahul5116@outlook.com>
2025-12-17 16:28:53 +0530

    Refactor Scope&Global.py to enhance clarity of global and local variable usage

--- Scope&Global.py
+++ Scope&Global.py
@@ -1,3 +1,11 @@
 # Global variable
 x='Rahul'
-
+def func():
+    # Local variable
+    y='Kumar'
+    print(x)
+    print(y)
+func()
+print(x)
+# print(y) # This will raise an error because y is not defined in the global scope
+# The above code demonstrates the concept of scope in Python.
