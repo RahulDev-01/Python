1c9217a3f7e89963c773c2a49e47737ef78ddb33
S.Rahul <s.rahul5116@outlook.com>
2025-12-17 16:30:06 +0530

    Refactor func in Scope&Global.py to accept a parameter and clarify variable scope

--- Scope&Global.py
+++ Scope&Global.py
@@ -1,11 +1,10 @@
 # Global variable
 x='Rahul'
-def func():
+def func(name):
     # Local variable
-    y='Kumar'
+    x=name
     print(x)
-    print(y)
-func()
 print(x)
-# print(y) # This will raise an error because y is not defined in the global scope
-# The above code demonstrates the concept of scope in Python.
+func('Changed')
+# print(x)
+
